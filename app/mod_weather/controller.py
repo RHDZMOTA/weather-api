83e44d206a82f5d3493b2fd9ede962723492e952
rhdzmota <rhdzmota@mxquants.com>
2017-11-24 16:05:29 -0600

    quickfix using jsonify

--- app/mod_weather/controller.py
+++ app/mod_weather/controller.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Blueprint, request, render_template, flash, jsonify
+from flask import Blueprint, request, render_template, flash, jsonify, Response
 from app import app
 
 from app.mod_weather.service import WeatherManager
@@ -17,9 +17,9 @@ def weather_hello():
 def get_weather_by_coordinates():
     lat = request.args.get("lat") if request.method == "GET" else request.get_json().get("lat")
     lng = request.args.get("lng") if request.method == "GET" else request.get_json().get("lng")
-    return WeatherManager().get_from_coordinates(lat, lng)
+    return jsonify(eval(WeatherManager().get_from_coordinates(lat, lng))) #Response(WeatherManager().get_from_coordinates(lat, lng), mimetype='application/json')
 
 
 @mod_weather.route('/city/<city>', methods=['GET', 'POST'])
 def get_weather_by_city(city):
-    return WeatherManager().get_from_city(str(city))
+    return jsonify(eval(WeatherManager().get_from_city(str(city))))
